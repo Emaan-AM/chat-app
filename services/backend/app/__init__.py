1ac6ce4160c9f59c453a0623b23148a07a2fec76
Emaan-AM <eamueed@gmail.com>
2025-10-28 00:09:19 +0500

    fixed cicd pipeline (flake8)

--- services/backend/app/__init__.py
+++ services/backend/app/__init__.py
@@ -5,36 +5,43 @@ from flask import Flask
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 
-
 load_dotenv()
 cors = CORS()
 db = SQLAlchemy()
- 
-def create_app(script_info=None):
 
-    #set flask app settings from environmental variables set in docker-compose
+
+def create_app(script_info=None):
+    """Create and configure the Flask app."""
+
+    # set flask app settings from environmental variables set in docker-compose
     app_settings = os.getenv("BACKEND_APP_SETTINGS")
 
-    #instantiate app
+    # instantiate app
     app = Flask(__name__)
-    
-    #set configs
+
+    # set configs
     app.config.from_object(app_settings)
 
-    #set extensions
+    # set extensions
     cors.init_app(
         app,
-        resources= {r"*": {"origins": "*", "allow_headers": "*", "expose_headers": "*"}},
-        supports_credentials= True
+        resources={
+            r"*": {
+                "origins": "*",
+                "allow_headers": "*",
+                "expose_headers": "*",
+            },
+        },
+        supports_credentials=True,
     )
     db.init_app(app)
 
-    #register api
+    # register api
     from app.api import api
     api.init_app(app)
 
     @app.shell_context_processor
     def ctx():
-        return {"app":app,"db":db}
+        return {"app": app, "db": db}
 
     return app
--- services/backend/helper/crud.py
+++ services/backend/helper/crud.py
@@ -2,26 +2,31 @@ from app import db
 from helper.models import Message
 from sqlalchemy import asc
 
-def get_all_messages():
-    """get all messages in the db"""
 
+def get_all_messages():
+    """Get all messages in the db"""
     messages = Message.query.order_by(asc(Message.date)).all()
     return messages
 
-def add_message(text):
-    "add a message to the db"
 
+def add_message(text):
+    """Add a message to the db"""
     if text.strip() == "":
-        return {"status":"error","message":"text is empty"}
+        return {"status": "error", "message": "text is empty"}
 
     message = Message(text=text)
     db.session.add(message)
     db.session.commit()
-    message_json = {"status":"ok",
-                    "message":{"id":message.id,
-                                "text":message.text,
-                                "date":f"{message.date.year}-{message.date.month}-{message.date.day}"
-                                }
-                    }
-    
+
+    message_json = {
+        "status": "ok",
+        "message": {
+            "id": message.id,
+            "text": message.text,
+            "date": f"{message.date.year}-"
+                    f"{message.date.month}-"
+                    f"{message.date.day}"
+        }
+    }
+
     return message_json
